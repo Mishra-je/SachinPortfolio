a3e313b6697bcd470a1cf1efbfe4a9c4cf82f3e4
Airbnb-Project <skumarmishra579@gmail.com>
2024-06-19 11:50:18 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-gradient-to-r from-black to-blue-900 ... overflow-x-hidden'>
+    
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-white text-2xl font-bold"'>Hello folks! This is </p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        Sachin Mishra
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a MERN-Stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        I'm a MERN stack developers play a crucial role in modern web development, combining their expertise in front-end and back-end technologies to create seamless and efficient web applications.
+        </p>
+        <div>
+          <Link to='work' smooth={true} duration={500}>
+            <button className='text-white group border-2 border-blue-500 px-6 py-3 my-2 flex items-center bg-blue-500 hover:bg-blue-900 hover:border-blue-900 transition duration-700'>
+              View Work
+              <span className='group-hover:rotate-90 transition duration-700'>
+                <HiArrowNarrowRight className='ml-3' />
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+import todo from '../assets/projects/todo.png'
+import Music from '../assets/projects/MusicPlayer.png'
+import airbnb from '../assets/projects/airbnb.png'
+import quiz from '../assets/projects/quiz.png'
+import chair from '../assets/projects/chair.png'
+import countryproject from '../assets/projects/countryproject.jpeg'
+
+
+export const data=[
+    {
+        id:1,
+        name:"Country-guide",
+        image:countryproject,
+        github:"https://github.com/Mishra-je",
+        live:"https://mishra-je.github.io/Country-Guide-Project/",
+    },
+    {
+        id:2,
+        name:"To-Do List",
+        image:todo,
+        github:"https://github.com/Mishra-je",
+        live:"https://mishra-je.github.io/To-do-list-Project/",
+    },
+    {
+        id:3,
+        name:"Music Player",
+        image:Music ,
+        github:"https://github.com/Mishra-je",
+        // live:"https://bhardwajjshubh.github.io/Music-Player/",
+    },
+    {
+        id:4,
+        name:"Airbnb Clone",
+        image:airbnb,
+        github:"https://github.com/Mishra-je",
+        live:"https://mishra-je.github.io/Airbnb-Project-Tailwind/",
+    },
+    {
+        id:5,
+        name:"Quiz",
+        image:quiz,
+        github:"https://github.com/Mishra-je",
+        live:"https://mishra-je.github.io/Quiz-Project/",
+    },
+    {
+        id:6,
+        name:"Modern Chair",
+        image:chair,
+        github:"https://github.com/Mishra-je",
+        live:"https://mishra-je.github.io/Modern-Chair/",
+    },
+    
+    
+
+]
